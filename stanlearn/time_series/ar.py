1e746ec09c95e710b12811b6a8db4013d578bf7f
RJTK <ryan@kinnear.ca>
2020-06-11 12:23:44 -0400

    Getting started on some time series models.

---
+++ stanlearn/time_series/ar.py
@@ -0,0 +1,143 @@
+from copy import deepcopy
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.base import RegressorMixin, BaseEstimator
+from sklearn.exceptions import NotFittedError
+from sklearn.preprocessing import StandardScaler
+
+from stanlearn.base import StanCacheMixin
+
+
+class BayesAR(BaseEstimator, RegressorMixin, StanCacheMixin):
+    def __init__(self, n_jobs=-1, warmup=1000, samples_per_chain=1000,
+                 n_chains=4, normalize=True, max_samples_mem=500):
+        BaseEstimator.__init__(self)
+
+        self.stan_model, self.predict_model = self._load_compiled_models()
+
+        self.stan_fitting_kwargs = {"chains": n_chains,
+                                    "iter": samples_per_chain + warmup,
+                                    "warmup": warmup, "init": "random",
+                                    "init_r": 1.0, "n_jobs": n_jobs,
+                                    "control": {"metric": "diag_e",
+                                                "adapt_delta": 0.9}}
+
+        self._fit_results = None
+        self.normalize = normalize
+        self.max_samples_mem = max_samples_mem
+
+        if normalize:
+            self._y_ss = StandardScaler(with_mean=False)
+            self._X_ss = StandardScaler()
+        return
+
+    def _posterior(self, X, **stan_fitting_kwargs):
+        N, M = X.shape
+
+        if self.normalize:
+            X = self._X_ss.transform(X)
+
+        # y0, beta, sigma, nu = self._get_param_posterior()
+
+        mem_samples = len(y0) * 8 * N / 1e6
+        ss = int(1 + (mem_samples // self.max_samples_mem))  # Subsample
+        K = len(y0[::ss])
+
+        data = {"N": N, "M": M, "K": K}
+        fit_kwargs = self._setup_predict_kwargs(data, stan_fitting_kwargs)
+        fit_kwargs["iter"] = 1
+        fit_kwargs["chains"] = 1
+
+        predictions = self.predict_model.sampling(**fit_kwargs,
+                                                  algorithm="Fixed_param")
+        y_samples = predictions.extract("y")["y"][0, ...]
+        y_hat = predictions.extract("y_hat")["y_hat"].ravel()
+
+        if self.normalize:
+            y_samples = np.vstack([self._y_ss.inverse_transform(y_s)
+                                   for y_s in y_samples])
+            y_hat = self._y_ss.inverse_transform(y_hat)
+        return y_hat, y_samples
+
+    def _get_aram_posterior(self):
+        if self._fit_results is None:
+            raise NotFittedError("Model isn't fit!")
+        df = self._fit_results.to_dataframe()
+        return
+
+    def fit(self, X, y, sample_weight=None, **stan_fitting_kwargs):
+        """
+        "Fit" the model, that is, sample from the posterior.
+
+        params:
+            X (n_examples, m_features): Regressors
+            y (n_examples): The targets
+            sample_weight: NotImplemented
+            stan_fitting_kwargs: To be passed to pystan's .sampling method
+        """
+        if sample_weight is not None:
+            raise NotImplementedError("sampling weighting is not implemented.")
+        N, M = X.shape
+
+        if self.normalize:
+            y = self._y_ss.fit_transform(y)
+            X = self._X_ss.fit_transform(X)
+
+        y = y.ravel()
+        data = {"N": N, "M": M, "X": X, "y": y}
+
+        fit_kwargs = self._setup_predict_kwargs(data, stan_fitting_kwargs)
+        self._fit_results = self.stan_model.sampling(**fit_kwargs)
+        return
+
+    def predict(self, X, ret_posterior=False, **stan_fitting_kwargs):
+        """
+        Produce samples from the predictive distribution.  This can be
+        used for either prior predictive checks or for posterior
+        predictions.
+
+        params:
+            X (n_examples, m_features): Regressors
+            ret_posterior: Whether or not to return all the
+                posterior samples.  If false, we only return the
+                posterior mean, which is dramatically faster.
+            stan_fitting_kwargs: kwargs for pystan's sampling method.
+
+        returns:
+            y_hat (n_examples), y_samples (k_samples, n_examples) -- (if
+                ret_posterior=True)
+            y_hat (n_examples) -- (otherwise)
+        """
+        if ret_posterior:
+            y_hat, y_samples = self._posterior(X, **stan_fitting_kwargs)
+            return y_hat, y_samples
+        else:
+            return y_hat
+    
+
+    def plot_posterior_params(self, show=False):
+        """
+        A helper method to plot the posterior parameter distribution.
+        Will raise an error if .fit hasn't been called.
+        """
+        param_df = self._fit_results.to_dataframe()
+
+        fig, ax = plt.subplots(1, 1)
+        ax.set_title(
+            "Parameter Posterior Marginals: "
+            "$y \\sim \\mathcal{T}(\\nu, y_0 + X\\beta, \\sigma)$")
+        sns.boxplot(
+            data=param_df.melt(
+                value_name="Posterior Samples",
+                var_name="Parameter"),
+            x="Parameter", y="Posterior Samples", ax=ax)
+        if show:
+            plt.show()
+        return fig, ax
+
+
+if __name__ == "__main__":
+    ar = BayesAR()
---
+++ stanlearn/time_series/stan_models/BayesAR.stan
@@ -0,0 +1,62 @@
+/*
+ * A simple AR(p) model.
+ */
+
+functions {
+  vector step_up_recursion(vector g){
+    /*
+     * Maps a vector of p reflection coefficients g (|g| < 1) to a
+     * stable sequence b of AR coefficients.
+     */
+    int p = dims(g);  // Model order
+    vector[p + 1] b;  // AR Coefficients
+    vector[p + 1] b_cpy;  // Memory
+
+    b[1] = 1;
+    for(k in 1:p){
+        b_cpy = b;
+        for(tau in 2:k){
+          b[tau] = b_cpy[tau] + g[k] * b_cpy[k - tau + 1];
+        }
+        b[k + 1] = g[k];
+      }
+
+    return -b[2:];
+  }
+}
+
+data {
+  int<lower=1> N;  // Number of examples
+  vector[N] y;  // the data series y(t)
+}
+
+transformed data {
+}
+
+parameters {
+  real mu;  // Mean value
+  real y0;  // Initial value
+  vector<lower=-1, upper=1>[1] g;  // Reflection coefficients
+  real<lower=0> sigma;  // noise level
+  real<lower=0> lam;  // variance of reflection coefficients
+}
+
+transformed parameters {
+  vector[1] b;  // AR Coefficients
+
+  b = step_up_recursion(g);
+}
+
+model {
+  sigma ~ normal(0, 1);
+  lam ~ normal(0, 1);
+  g ~ normal(0, lam);
+  mu ~ normal(0, 1);
+  y0 ~ normal(0, 1);
+
+  y[1] ~ normal(mu + y0, sigma);
+  y[2:N] ~ normal(mu + b * y[1:N - 1], sigma);
+}
+
+generated quantities {
+}
