acebdaf319b27abf0873b09e03265558cd83fbf9
RJTK <ryan@kinnear.ca>
2020-06-12 23:12:48 -0400

    Fixed step up recursion

--- stanlearn/time_series/ar.py
+++ stanlearn/time_series/ar.py
@@ -3,7 +3,10 @@ from copy import deepcopy
 
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib import patches
+
 import numpy as np
+from numpy.polynomial.polynomial import polyroots
 
 from sklearn.base import RegressorMixin, BaseEstimator
 from sklearn.exceptions import NotFittedError
@@ -19,11 +22,12 @@ except NameError:  # no __file__ when interactive
 
 
 class BayesAR(BaseEstimator, RegressorMixin, StanCacheMixin):
-    def __init__(self, n_jobs=-1, warmup=1000, samples_per_chain=1000,
+    def __init__(self, p=1, n_jobs=-1, warmup=1000, samples_per_chain=1000,
                  n_chains=4, normalize=True, max_samples_mem=500):
         BaseEstimator.__init__(self)
         StanCacheMixin.__init__(self, MODEL_DIR)
 
+        self.p = p  # The model order
         self.stan_model, self.predict_model = self._load_compiled_models()
 
         self.stan_fitting_kwargs = {"chains": n_chains,
@@ -75,7 +79,7 @@ class BayesAR(BaseEstimator, RegressorMixin, StanCacheMixin):
         df = self._fit_results.to_dataframe()
         return
 
-    def fit(self, X, y=None, p=1, sample_weight=None, **stan_fitting_kwargs):
+    def fit(self, X, y=None, sample_weight=None, **stan_fitting_kwargs):
         """
         "Fit" the model, that is, sample from the posterior.
 
@@ -96,7 +100,7 @@ class BayesAR(BaseEstimator, RegressorMixin, StanCacheMixin):
 
         X = X.ravel()
         # data = {"N": N, "M": M, "X": X, "y": y}
-        data = {"T": T, "p": p, "y": X}
+        data = {"T": T, "p": self.p, "y": X}
 
         fit_kwargs = self._setup_predict_kwargs(data, stan_fitting_kwargs)
         self._fit_results = self.stan_model.sampling(**fit_kwargs)
@@ -120,12 +124,14 @@ class BayesAR(BaseEstimator, RegressorMixin, StanCacheMixin):
                 ret_posterior=True)
             y_hat (n_examples) -- (otherwise)
         """
-        if ret_posterior:
-            y_hat, y_samples = self._posterior(X, **stan_fitting_kwargs)
-            return y_hat, y_samples
-        else:
-            return y_hat
-    
+        raise NotImplementedError
+
+    def get_ppc(self):
+        """
+        A built in PPC for every fit.
+        """
+        y_ppc = self._fit_results.extract("y_ppc")["y_ppc"]
+        return y_ppc
 
     def plot_posterior_params(self, show=False):
         """
@@ -133,43 +139,48 @@ class BayesAR(BaseEstimator, RegressorMixin, StanCacheMixin):
         Will raise an error if .fit hasn't been called.
         """
         param_df = self._fit_results.to_dataframe()
+        p = self.p
 
-        fig, ax = plt.subplots(1, 1)
-        ax.set_title(
-            "Parameter Posterior Marginals: "
-            "$y \\sim \\mathcal{T}(\\nu, y_0 + X\\beta, \\sigma)$")
+        params = (["sigma", "lam", "mu"] +
+                  [f"b[{tau}]"for tau in range(1, p + 1)])
+        names = (["$\sigma$", "$\lambda$", "$\mu$"] + 
+                  [f"$b_{tau}$" for tau in range(1, p + 1)])
+        rename = {frm: to for frm, to in zip(params, names)}
+
+        param_df = param_df.loc[:, params]\
+            .rename(rename, axis=1)
+
+        fig, axes = plt.subplots(1, 2)
+        ax = axes.ravel()
+
+        # Plot parameters
+        fig.suptitle("$y(t) \sim \mathcal{N}(\mu + \sum_{\\tau = 1}^p "
+                     "b_\\tau y(t - \\tau), \sigma^2); "
+                     "b_\\tau \sim \mathcal{N}(0, \lambda^2)$")
+
+        ax[0].set_title("Parameter Posteriors")
         sns.boxplot(
             data=param_df.melt(
                 value_name="Posterior Samples",
                 var_name="Parameter"),
-            x="Parameter", y="Posterior Samples", ax=ax)
+            x="Parameter", y="Posterior Samples", ax=ax[0])
+
+        # Z-plot
+        roots = self._compute_roots(param_df.iloc[:, 3:].to_numpy())
+
+        uc = patches.Circle((0, 0), radius=1, fill=False,
+                            color='black', linestyle='dashed')
+        ax[1].add_patch(uc)
+        ax[1].scatter(roots.real, roots.imag, color="#0072B2",
+                      marker="x", alpha=0.2)
+        ax[1].set_title("System Poles")
+
         if show:
             plt.show()
         return fig, ax
 
-
-if __name__ == "__main__":
-    ar = BayesAR(normalize=False)
-    T = 100
-    v = 0.25 * np.random.normal(size=T)
-    y = np.array(v)
-    b1 = 0.6
-    b2 = -0.8
-
-    for t in range(2, T):
-        y[t] = b1 * y[t - 1] + b2 * y[t - 2] + v[t]
-
-    y = y.reshape(-1, 1)
-    ar.fit(y, p=8)
-
-    y_ppc = ar._fit_results.extract("y_ppc")["y_ppc"]
-
-    plt.plot(y_ppc[::10].T, linewidth=0.5, color="m", alpha=0.25)
-    plt.plot(y.ravel(), linewidth=2.0, color="b", alpha=0.8, label="y")
-    plt.plot(np.mean(y_ppc, axis=0), linewidth=2.0, color="r",
-             alpha=0.8, label="y\_ppc")
-    plt.xlabel("$t$")
-    plt.ylabel("$y$")
-    plt.title("AR(p) model PPC")
-    plt.legend()
-    plt.show()
+    def _compute_roots(self, b):
+        roots = []
+        for bi in b:
+            roots.append(polyroots(np.append(-bi[::-1], 1)))
+        return np.vstack(roots)
--- stanlearn/time_series/stan_models/BayesAR_model.stan
+++ stanlearn/time_series/stan_models/BayesAR_model.stan
@@ -3,25 +3,25 @@
  */
 
 functions {
-  vector step_up_recursion(vector g){
+  vector step_up(vector g){
     /*
      * Maps a vector of p reflection coefficients g (|g| < 1) to a
      * stable sequence b of AR coefficients.
      */
     int p = dims(g)[1];  // Model order
-    vector[p + 1] b;  // AR Coefficients
-    vector[p + 1] b_cpy;  // Memory
+    vector[p] b;  // AR Coefficients
+    vector[p] b_cpy;  // Memory
 
-    b[1] = 1;
-    for(k in 1:p){
+    b[1] = g[1];
+    for(k in 1:p - 1){
         b_cpy = b;
-        for(tau in 2:k){
-          b[tau] = b_cpy[tau] + g[k] * b_cpy[k - tau + 1];
+        for(tau in 1:k){
+          b[tau] = b_cpy[tau] + g[k + 1] * b_cpy[k - tau + 1];
         }
-        b[k + 1] = g[k];
+        b[k + 1] = g[k + 1];
       }
 
-    return -b[2:];
+    return -b;
   }
 }
 
@@ -45,24 +45,24 @@ parameters {
 transformed parameters {
   vector[p] b;  // AR Coefficients
 
-  b = step_up_recursion(g);
+  b = step_up(g);
 }
 
 model {
-  sigma ~ normal(0, 1);
-  lam ~ normal(0, 1);
-  g ~ normal(0, lam);
+  sigma ~ normal(0, 5);
+  lam ~ normal(0, 2);
+  g ~ normal(0, lam^2);
   mu ~ normal(0, 1);
   y0 ~ normal(0, 1);
 
   for(t in 1:p)
-    y[t] ~ normal(mu + y0[t], sigma);
+    y[t] ~ normal(mu + y0[t], sigma^2);
 
   for(t in p + 1:T){
     real y_hat = 0;
     for(tau in 1:p)
       y_hat += b[tau] * y[t - tau];
-    y[t] ~ normal(mu + y_hat, sigma);
+    y[t] ~ normal(mu + y_hat, sigma^2);
   }
 }
 
@@ -70,12 +70,12 @@ generated quantities {
   vector[T] y_ppc;
 
   for(t in 1:p)
-    y_ppc[t] = normal_rng(mu + y0[t], sigma);
+    y_ppc[t] = normal_rng(mu + y0[t], sigma^2);
 
   for(t in p + 1:T){
     real y_hat = 0;
     for(tau in 1:p)
       y_hat += b[tau] * y[t - tau];
-    y_ppc[t] = normal_rng(mu + y_hat, sigma);
+    y_ppc[t] = normal_rng(mu + y_hat, sigma^2);
   }
 }
